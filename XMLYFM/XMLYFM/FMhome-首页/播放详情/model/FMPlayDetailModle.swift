d5b5a65ac8824bb78ec2b653295653795d2f5837
张传伟 <zhangchuanwei@yikaiye.com>
2018-10-15 17:18:55 +0800

    专辑详细

---
+++ XMLYFM/XMLYFM/FMhome-首页/播放详情/model/FMPlayDetailModle.swift
@@ -0,0 +1,13 @@
+//
+//  FMPlayDetailModle.swift
+//  XMLYFM
+//
+//  Created by 张传伟 on 2018/10/15.
+//  Copyright © 2018年 张传伟. All rights reserved.
+//
+
+import UIKit
+
+class FMPlayDetailModle: NSObject {
+
+}
